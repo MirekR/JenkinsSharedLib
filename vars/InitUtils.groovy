3e6dbd369e9d1644012a828f1b986589078be647
Mirek Rousal <mirekrousal@users-mbp.home>
2018-02-06 22:46:54 +0000

    lower case utils

--- vars/InitUtils.groovy
+++ vars/InitUtils.groovy
@@ -1,3 +1,11 @@
 def doStuff() {
   sh "echo 'Stuff'"
 }
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+}
